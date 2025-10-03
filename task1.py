4b5deac4512f234e276be256a0632ae62487eae8
triss-minyard <triss.minyard@gmail.com>
2025-10-03 11:20:34 +0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,7 @@
 num = 1
 n = int(input("Введите число n: "))
-while n >=16 num**2:
+while n >=num**2:
     num+=1
-# while True:
-#     if num**2 > n:
-#         print()
-#         break
-#     num+=1
 print("Первое натуральное число, квадрат которого больше n ", num)
+
 
